05a709ba2661acd1e4500f010f2958ad8770c2e1
SamKlep <shamdemann23@gmail.com>
2020-10-20 14:32:51 -0500

    Custom async/await middleware.

--- controllers/fungus.js
+++ controllers/fungus.js
@@ -1,103 +1,75 @@
 const path = require('path')
-
+const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
 const Fungus = require('../models/Fungus.js')
 
 // @desc        Get all fungi
 // @route       GET /api/v1/fungus
 // @access      Public
-exports.getFungi = async (req, res, next) => {
-  try {
-    const fungi = await Fungus.find()
+exports.getFungi = asyncHandler(async (req, res, next) => {
+  const fungi = await Fungus.find()
 
-    res.status(200).json({
-      success: true,
-      count: fungi.length,
-      data: fungi,
-    })
-  } catch (err) {
-    res.status(400).json({
-      success: false,
-    })
-  }
-}
+  res.status(200).json({
+    success: true,
+    count: fungi.length,
+    data: fungi,
+  })
+})
 
 // @desc        Get single fungus
 // @route       GET /api/v1/fungus/:id
 // @access      Public
-exports.getFungus = async (req, res, next) => {
-  try {
-    const fungus = await Fungus.findById(req.params.id)
+exports.getFungus = asyncHandler(async (req, res, next) => {
+  const fungus = await Fungus.findById(req.params.id)
 
-    if (!fungus) {
-      return res.status(400).json({
-        success: false,
-      })
-    }
-
-    res.status(200).json({
-      success: true,
-      data: fungus,
-    })
-  } catch (err) {
-    // res.status(400).json({
-    //   success: false,
-    // })
-    next(err)
+  if (!fungus) {
+    return next(
+      new ErrorResponse(`Fungus not found with id of ${req.params.id}`, 404)
+    )
   }
-}
+
+  res.status(200).json({
+    success: true,
+    data: fungus,
+  })
+})
 
 // @desc        Create new fungus
 // @route       POST /api/v1/fungus
 // @access      Private
-exports.createFungus = async (req, res, next) => {
-  try {
-    const fungus = await Fungus.create(req.body)
+exports.createFungus = asyncHandler(async (req, res, next) => {
+  const fungus = await Fungus.create(req.body)
 
-    res.status(200).json({ success: true, data: fungus })
-  } catch (err) {
-    res.status(400).json({ success: false })
-  }
-}
+  res.status(200).json({ success: true, data: fungus })
+})
 
 // @desc        Update fungus
 // @route       PUT /api/v1/fungus/:id
 // @access      Private
-exports.updateFungus = async (req, res, next) => {
-  try {
-    const fungus = await Fungus.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    })
+exports.updateFungus = asyncHandler(async (req, res, next) => {
+  const fungus = await Fungus.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
 
-    if (!fungus) {
-      return res.status(400).json({
-        success: false,
-      })
-    }
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-    })
+  if (!fungus) {
+    return next(
+      new ErrorResponse(`Fungus not found with id of ${req.params.id}`, 404)
+    )
   }
-}
+})
 
 // @desc        Delete fungus
 // @route       DELETE /api/v1/fungus/:id
 // @access      Private
-exports.deleteFungus = async (req, res, next) => {
-  try {
-    const fungus = await Fungus.findByIdAndDelete(req.params.id)
+exports.deleteFungus = asyncHandler(async (req, res, next) => {
+  const fungus = await Fungus.findByIdAndDelete(req.params.id)
 
-    if (!fungus) {
-      return res.status(400).json({
-        success: false,
-      })
-    }
-
-    res.status(200).json({ success: true, data: {} })
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-    })
+  if (!fungus) {
+    return next(
+      new ErrorResponse(`Fungus not found with id of ${req.params.id}`, 404)
+    )
   }
-}
+
+  res.status(200).json({ success: true, data: {} })
+})
