3f0f65b4796173ff237b7993e4b93c0a5c09acbb
SamKlep <shamdemann23@gmail.com>
2020-10-20 14:43:23 -0500

    Slugify.

--- models/Fungus.js
+++ models/Fungus.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 const FungusSchema = new mongoose.Schema({
   name: {
@@ -7,7 +8,7 @@ const FungusSchema = new mongoose.Schema({
   subName: {
     type: String,
   },
-
+  slug: String,
   family: {
     type: String,
   },
@@ -35,6 +36,12 @@ const FungusSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+})
+
+// Create fungus slug from name
+FungusSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true })
+  next()
 })
 
 module.exports = mongoose.model('Fungus', FungusSchema)
