d82a76a8709b0fe20819b543baa14bd528c7cbb2
SamKlep <shamdemann23@gmail.com>
2020-10-26 17:35:52 -0500

    404 page styling.

--- client/src/components/pages/NotFound.js
+++ client/src/components/pages/NotFound.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <article className='not-found container'>
+    <article className='not-found container mt-5'>
       <h1>404!</h1>
       <p>
         Content not found.
