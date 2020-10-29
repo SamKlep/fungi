b483d2df4fed8ca482183ed938afc8235940c920
SamKlep <shamdemann23@gmail.com>
2020-10-29 16:38:58 -0500

    Heroku post build.

--- server.js
+++ server.js
@@ -25,6 +25,15 @@ const fungus = require('./routes/fungus')
 
 const app = express()
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build', 'public'))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+}
+
 // Body parser
 app.use(express.json())
 
