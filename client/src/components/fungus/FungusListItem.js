c4711b1d1b280949c4f5ecb260c20615be02b9c3
SamKlep <shamdemann23@gmail.com>
2020-10-29 22:02:49 -0500

    Reversed heroku changes.

--- client/src/components/fungus/FungusListItem.js
+++ client/src/components/fungus/FungusListItem.js
@@ -2,42 +2,42 @@ import React from 'react'
 import { Image, ListGroup } from 'react-bootstrap'
 import './fungus.css'
 
-const FungusListItem = ({ data }) => {
+const FungusListItem = ({ fungus }) => {
   return (
     <div className='container m-auto mb-5'>
       <div className='card2 justify-content-md-center '>
-        <Image style={{ overflow: 'hidden' }} alt='fungi' src={data.imgUrl} />
+        <Image style={{ overflow: 'hidden' }} alt='fungi' src={fungus.imgUrl} />
 
         <div className='card-body'>
-          <h1>{data.name}</h1>
+          <h1>{fungus.name}</h1>
 
           <ListGroup>
             <ListGroup.Item className='bg-light'>
               <strong>Family: </strong>
-              {data.family}
+              {fungus.family}
             </ListGroup.Item>
             <ListGroup.Item>
               <strong> Sub-Name: </strong>
-              {data.subname}
+              {fungus.subname}
             </ListGroup.Item>
             <ListGroup.Item className='bg-light'>
               <strong> Location: </strong>
-              {data.location}
+              {fungus.location}
             </ListGroup.Item>
             <ListGroup.Item>
-              {data.edible === true ? 'Edible' : 'Inedible'}
+              {fungus.edible === true ? 'Edible' : 'Inedible'}
             </ListGroup.Item>
             <ListGroup.Item className='bg-light'>
               <strong> Dimensions: </strong>
-              {data.dimensions}
+              {fungus.dimensions}
             </ListGroup.Item>
             <ListGroup.Item>
               <strong> Description: </strong>
-              {data.description}
+              {fungus.description}
             </ListGroup.Item>
             <ListGroup.Item className='bg-light'>
               <strong> Similar: </strong>
-              {data.similar}
+              {fungus.similar}
             </ListGroup.Item>
           </ListGroup>
         </div>
