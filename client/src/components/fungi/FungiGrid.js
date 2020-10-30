ffc7433178353f860c9b1ea7629075e82b65c188
SamKlep <shamdemann23@gmail.com>
2020-10-30 10:33:25 -0500

    Map in FungusGrid component.

--- client/src/components/fungi/FungiGrid.js
+++ client/src/components/fungi/FungiGrid.js
@@ -3,10 +3,12 @@ import './Explore.css'
 import FungusItem from './FungusItem'
 
 const FungiGrid = ({ fungi, isLoading }) => {
+  console.log(fungi.data)
+
   return isLoading ? (
     <h1>Loading...</h1>
   ) : (
-    <section className='cards pill-links'>
+    <section className='cards'>
       {fungi.data.map((fungus) => (
         <FungusItem key={fungus._id} fungus={fungus}></FungusItem>
       ))}
